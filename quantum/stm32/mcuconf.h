5ee2f4c60057302fa3c7c902087a04762262a634
Johannes <you@example.com>
2020-03-19 19:21:06 +0100

    audio-arm-pwm: enable PWMD1 which is used as software/hardware driver by quantum/audio/audio_arm_pwm.c

--- quantum/stm32/mcuconf.h
+++ quantum/stm32/mcuconf.h
@@ -187,7 +187,8 @@
  * PWM driver system settings.
  */
 #define STM32_PWM_USE_ADVANCED FALSE
-#define STM32_PWM_USE_TIM1 FALSE
+/* PWMD1 used by quantum/audio/audio_arm_pwm.c */
+#define STM32_PWM_USE_TIM1 TRUE
 #define STM32_PWM_USE_TIM2 FALSE
 #define STM32_PWM_USE_TIM3 TRUE
 #define STM32_PWM_USE_TIM4 TRUE
